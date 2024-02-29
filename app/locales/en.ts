bb888202727cfb6df5a0823511451e83cc5a29fc
Sergio Xalambrí <hello@sergiodxa.com>
2024-02-29 01:51:23 -0500

    Create file with translations

---
+++ app/locales/en.ts
@@ -0,0 +1,4 @@
+export default {
+  title: "remix-i18next (en)",
+  description: "A Remix + Vite + remix-i18next example",
+};
---
+++ app/locales/es.ts
@@ -0,0 +1,4 @@
+export default {
+  title: "remix-i18next (es)",
+  description: "Un ejemplo de Remix + Vite + remix-i18next",
+};
