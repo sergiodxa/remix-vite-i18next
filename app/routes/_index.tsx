e16c8f0f42ac1afdbb890b3e1ed9f7044759ea4d
Sergio Xalambrí <hello@sergiodxa.com>
2024-02-29 01:52:26 -0500

    Use getFixedT and useTranslation in /

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,11 @@
-import type { MetaFunction } from "@remix-run/node";
+import {
+  json,
+  type LoaderFunctionArgs,
+  type MetaFunction,
+} from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { useTranslation } from "react-i18next";
+import i18nServer from "~/modules/i18n.server";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,10 +14,20 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export async function loader({ request }: LoaderFunctionArgs) {
+  const t = await i18nServer.getFixedT(request);
+  return json({ description: t("description") });
+}
+
 export default function Index() {
+  const { t } = useTranslation();
+  const { description } = useLoaderData<typeof loader>();
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+      <h1>{t("title")}</h1>
+      <p>{description}</p>
+
       <ul>
         <li>
           <a
